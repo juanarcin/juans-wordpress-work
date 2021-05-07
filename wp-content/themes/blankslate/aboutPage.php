9d1adfa91475dcc933c084110baddfeb9be14cbe
juanarcin <juanarcin@gmail.com>
2021-05-07 15:00:23 -0700

    completed wordpress theme

---
+++ wp-content/themes/blankslate/aboutPage.php
@@ -0,0 +1,81 @@
+<?php /* Template Name: About */ ?>
+<?php get_header(); ?>
+
+<section class="panel" id="about">
+	<content>
+		<div class="about-text">
+			<h3>Hi There!</h3>
+			<p>My Name is Juan and I'm a Front End Developer who's been coding professionally for <span id="coding-history"></span></p>
+			<p>In my past roles I've worked closely with both design and tech teams allowing me to experience the best of both worlds.</p>
+			<p>I've helped small startups grow and large agencies continue rocking! This has allowed me to work with teams with as little as 3 and as many as 15+ engineers.</p>
+			<p>The projects I've participated in have included, Wordpress Sites (custom themes and maintenance), Single Page Applications, Landing Pages, Multi Step Forms, Ecommerce Sites, Responsive Emails, and tons of websites!</p>
+			<p>To learn more about me feel free to visit my  LinkedIn Profile.</p>
+		</div>
+		<ul class="skill-column">
+			<li>javascript
+				<ul>
+					<li class="skill active" data-skill-type="js">AngularJs</li>
+					<li class="skill active" data-skill-type="js">Javascript</li>
+					<li class="skill active" data-skill-type="js">jQuery</li>
+					<li class="skill active" data-skill-type="js">ReactJs</li>
+				</ul>
+			</li>
+			<li>layout
+				<ul>
+					<li class="skill" data-skill-type="layout">Bootstrap</li>
+					<li class="skill" data-skill-type="layout">CSS3</li>
+					<li class="skill" data-skill-type="layout">FlexBox</li>
+					<li class="skill" data-skill-type="layout">HTML5</li>
+					<li class="skill" data-skill-type="layout">Less/Sass</li>
+					<li class="skill" data-skill-type="layout">Responsive Design</li>
+					<li class="skill" data-skill-type="layout">Zurb Foundation</li>
+				</ul>
+			</li>
+			<li>ecommerce
+				<ul>
+					<li class="skill" data-skill-type="ecommerce">Ecommerce</li>
+					<li class="skill" data-skill-type="ecommerce">Paypal</li>
+					<li class="skill" data-skill-type="ecommerce">Stripe</li>
+					<li class="skill" data-skill-type="ecommerce">Woo-Commerce</li>
+				</ul>
+			</li>
+		</ul>
+		<ul class="skill-column">
+			<li>themes
+				<ul>
+					<li class="skill" data-skill-type="themes">Shopify Themes</li>
+					<li class="skill" data-skill-type="themes">WordPress Themes</li>
+				</ul>
+			</li>
+			<li>build
+				<ul>
+					<li class="skill" data-skill-type="build">NPM</li>
+					<li class="skill" data-skill-type="build">WebPack</li>
+				</ul>
+			</li>
+			<li>version conrol
+				<ul>
+					<li class="skill" data-skill-type="vc">GitHub</li>
+					<li class="skill" data-skill-type="vc">Mercurial</li>
+				</ul>
+			</li>
+			<li>design
+				<ul>
+					<li class="skill" data-skill-type="design">Fireworks</li>
+					<li class="skill" data-skill-type="design">Gimp</li>
+					<li class="skill" data-skill-type="design">Photoshop</li>
+				</ul>
+			</li>
+			<li>other
+				<ul>
+					<li class="skill" data-skill-type="extra">API</li>
+					<li class="skill" data-skill-type="extra">MVC</li>
+					<li class="skill" data-skill-type="extra">Redux</li>
+					<li class="skill" data-skill-type="extra">SPA</li>
+				</ul>
+			</li>
+		</ul>
+	</content>
+
+</section>
+<?php get_footer(); ?>
--- wp-content/themes/blankslate/homePage.php
+++ wp-content/themes/blankslate/homePage.php
@@ -1,22 +1,21 @@
 <?php /* Template Name: HomeAnimation */ ?>
 <?php get_header(); ?>
 
-<section class="panel active" id="home">
-		
-		<div class="home-page">
-			<div class="intro">
-				<h2 id="hi">Hello</h2>
-				<p>I'm Juan Arciniega.
-				<span class="hidden-text">I build websites with love <i class="fa fa-heart" aria-hidden="true"></i></span></p>
-			</div>
-			<div class="video-background">
-				<div class="video-overlay bringIn"></div>
-				<div class="video-foreground">
-					<video playsinline="" autoplay="" muted="" loop="" id="bgvid">
-						<source src="https://www.juans.work/shared/background.mp4" type="video/mp4">
-					</video>
-				</div>
+<section class="panel active" id="home">		
+	<div class="home-page">
+		<div class="intro">
+			<h2 id="hi">Hello</h2>
+			<p>I'm Juan Arciniega.
+			<span class="hidden-text">I build websites with love <i class="fa fa-heart" aria-hidden="true"></i></span></p>
+		</div>
+		<div class="video-background">
+			<div class="video-overlay bringIn"></div>
+			<div class="video-foreground">
+				<video playsinline="" autoplay="" muted="" loop="" id="bgvid">
+					<source src="https://www.juans.work/shared/background.mp4" type="video/mp4">
+				</video>
 			</div>
 		</div>
-	</section>
-	<?php get_footer(); ?>
+	</div>
+</section>
+<?php get_footer(); ?>
